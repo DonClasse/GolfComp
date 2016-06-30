81e3aa893d2e31fb890178bc6477389c4ebf9325
DonClasse <DonClasse@DON>
2016-06-30 18:57:29 +0200

    Början

---
+++ GolfComp/bana.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GolfComp
+{
+    
+    class bana
+    {
+        public static List<string> BanName = new List<string>();
+        
+        public static void AddName(string namn)
+        {
+            
+            BanName.Add(namn);
+            
+            
+            //hej
+        }
+
+     
+     
+    
+    }
+
+  
+}
